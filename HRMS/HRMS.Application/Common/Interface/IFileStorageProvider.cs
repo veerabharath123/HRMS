ca644ec3407992d8f625abc61d2dc0548ee34998
veerabharath123 <veerab1029@gmail.com>
2025-09-28 21:07:47 +0530

    committing changes

--- HRMS/HRMS.Application/Common/Interface/IFileStorageProvider.cs
+++ HRMS/HRMS.Application/Common/Interface/IFileStorageProvider.cs
@@ -7,7 +7,5 @@ namespace HRMS.Application.Common.Interface
         Task<bool> UploadAsync(string filename, Stream fileStream, CancellationToken cancellationToken = default);
         Task<bool> DeleteAsync(string filename, CancellationToken cancellationToken = default);
         Task<Stream?> FetchAsync(string filename, CancellationToken cancellationToken = default);
-
-
     }
 }
