96c32759c4c0dc6185bac7674e5d575ee51179bd
veerabharath123 <veerab1029@gmail.com>
2025-10-10 21:16:02 +0530

    committing changes

--- HRMS/HRMS.WebApplication/Class/CustomSecurityHeader.cs
+++ HRMS/HRMS.WebApplication/Class/CustomSecurityHeader.cs
@@ -24,18 +24,16 @@ namespace HRMS.WebApplication.Class
             rng.GetBytes(nonceBytes);
             var nonce = Convert.ToBase64String(nonceBytes);
 
-            string CSP = string.Format("default-src 'self'; " +
+            string CSP = string.Format("default-src 'self'; base-uri 'self'; object-src 'none'; " +
                     "connect-src 'self' https://maps.googleapis.com https://*.googleapis.com https://*.google.com wss://widget-mediator.zopim.com wss://localhost:44346/ ;" +
                     "script-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/ https://maps.googleapis.com https://maps.gstatic.com https://cdn.datatables.net 'nonce-{0}'; " +
                     "style-src 'self' https://maps.googleapis.com https://maps.gstatic.com https://fonts.googleapis.com https://cdn.datatables.net https://cdnjs.cloudflare.com 'nonce-{0}' 'strict-dynamic'; " +
                     "style-src-elem 'self' https://maps.googleapis.com https://maps.gstatic.com https://fonts.googleapis.com https://cdn.datatables.net https://cdnjs.cloudflare.com 'nonce-{0}' 'strict-dynamic'; " +
                     "img-src 'self' https://maps.gstatic.com/ data: https://maps.gstatic.com; " +
                     "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; " +
-                    "form-action 'self';" +
-                    "media-src 'self';" +
+                    "form-action 'self'; media-src 'self';" +
                     "frame-src 'self' https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;" +
-                    "frame-ancestors 'none'; " +
-                    "block-all-mixed-content;",
+                    "frame-ancestors 'none'; upgrade-insecure-requests; block-all-mixed-content;",
                 nonce);
 
             context = SetHeaders(context, nonce, CSP);
