109ac40542b4505ec05db219d4d9552da4057501
veerabharath123 <veerab1029@gmail.com>
2025-11-04 13:42:26 +0530

    .

---
+++ HRMS/HRMS.Application/Common/Interface/ITemplateRenderer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMS.Application.Common.Interface
+{
+    public interface ITemplateRenderer
+    {
+        Task<string> RenderTemplateAsync(string templateName, object data);
+    }
+}
